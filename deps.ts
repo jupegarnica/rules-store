ecc2b559970030aaf762a670cb5304c954b23d78
jupegarnica <jupegarnica@gmail.com>
2021-04-10 12:25:05 +0200

    add update script

--- deps.ts
+++ deps.ts
@@ -1,2 +1,2 @@
-export { createHash } from "https://deno.land/std@0.81.0/hash/mod.ts";
-export { exists } from "https://deno.land/std@0.81.0/fs/mod.ts";
+export { createHash } from "https://deno.land/std@0.92.0/hash/mod.ts";
+export { exists } from "https://deno.land/std@0.92.0/fs/mod.ts";

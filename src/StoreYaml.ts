6faef04ee222a4c3ce1a9c79cb2e6c3ad915b1f6
jupegarnica <jupegarnica@gmail.com>
2021-04-12 16:49:54 +0200

    create StoreYaml

---
+++ src/StoreYaml.ts
@@ -0,0 +1,103 @@
+import { existsSync } from './deps.ts';
+import {
+    parse,
+    stringify,
+  } from "https://deno.land/std@0.92.0/encoding/yaml.ts";
+import { Store } from './Store.ts';
+
+
+import type {
+    Value,
+  } from './types.ts';
+/**
+ * A database in RAM with persistance plain text as JSON.
+ * For non persistance use Store
+ */
+export class StoreYaml extends Store {
+  /**
+   * The file path in which to store the data in.
+   */
+  private _storePath: string;
+  /**
+   * Create a new {Store} instance.
+   * If no custom path is given, it defaults to mainModulePath/.store.yaml
+   *
+   * @param storePath A custom path where to write data
+   */
+  constructor(storePath?: string) {
+    super();
+    this._storePath = storePath
+      ? storePath
+      : `${new URL('.store.yaml', Deno.mainModule).pathname}`;
+    this.load();
+  }
+  /**
+   * Return internal storePath.
+   */
+   public get storePath(): string {
+    return this._storePath;
+  }
+  /**
+   * Load stored data from disk into cache.
+   * Won't update cache values if hash in store file matches current cache file.
+   *
+   * @param storePath Custom file path used by read operation
+   * @param force Ignore hash comparison and force read
+   */
+  private load(storePath?: string, force = false): void {
+    if (!storePath) storePath = this._storePath;
+    else this._storePath = storePath;
+    if (!existsSync(storePath)) return;
+
+    // Load data from file.
+    const data = Deno.readFileSync(storePath);
+    const decoder = new TextDecoder('utf-8');
+    const decoded: Value = parse(decoder.decode(data));
+
+    // Reload probably not necessary.
+    if (!force && decoded._hash === this._dataHash) return;
+
+    // Store new data.
+    this._data = decoded.data;
+    this._lastKnownStoreHash = decoded._hash;
+
+    return;
+  }
+
+  /**
+   * Writes cached data to disk.
+   * Won't perform write if the last known hash from the store file
+   * matches the current cache hash.
+   *
+   * @param storePath Custom file path used by write operation
+   * @param force Ignore hashe comparison and force write
+   */
+  public write(storePath?: string, force = false): void {
+    // Write probably not necessary.
+    if (!force && this._lastKnownStoreHash === this._dataHash) {
+      return;
+    }
+    if (!storePath) storePath = this._storePath;
+
+    // Write data.
+    const data = stringify({
+      _hash: this._dataHash,
+      data: this._data,
+    });
+    const encoder = new TextEncoder();
+    return Deno.writeFileSync(storePath, encoder.encode(data));
+  }
+
+  /**
+   * Deletes a store file / directory.
+   *
+   * @param storePath Custom path used by delete operation. Defaults to the default storage file path
+   */
+  public deleteStore(storePath?: string): void {
+    if (!storePath) storePath = this._storePath;
+    if (!existsSync(storePath)) {
+      throw new Error(`${storePath} not exists`);
+    }
+    return Deno.removeSync(storePath);
+  }
+}
---
+++ tests/storeYaml.test.ts
@@ -0,0 +1,49 @@
+import { StoreYaml } from '../src/StoreYaml.ts';
+import { existsSync } from '../src/deps.ts';
+import { assertEquals, assertThrows } from './test_deps.ts';
+const testStorePath = '../test.store.yaml';
+
+// Persistance StoreYaml
+////////////////////////
+Deno.test('[StoreYaml] Empty DB should not be persisted', () => {
+  const db = new StoreYaml('./not-exiting.yaml');
+  db.write();
+
+  assertEquals(existsSync(db.storePath), false);
+});
+
+Deno.test('[StoreYaml] Write DB', async () => {
+  const db = new StoreYaml('testStorePath');
+
+  db.set('number', 5);
+  assertEquals(db.get('number'), 5);
+
+  db.write();
+
+  assertEquals(existsSync(db.storePath), true);
+
+  await Deno.remove(db.storePath);
+});
+
+Deno.test('[StoreYaml] DB load / write / delete store', () => {
+  const db = new StoreYaml(testStorePath);
+
+  db.set('number5', 5);
+  db.set('number10', 10);
+
+  db.write(testStorePath);
+
+  const db2 = new StoreYaml(testStorePath);
+
+  assertEquals(db2.get('number5'), 5);
+
+  assertEquals(existsSync(db.storePath), true);
+
+  db.deleteStore();
+  assertThrows(() => {
+    db2.deleteStore();
+  });
+
+  const exists = existsSync(db.storePath);
+  assertEquals(exists, false);
+});

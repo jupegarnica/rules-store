d4c0c2463a73ccaa8a2ea6a6b0baee5586ce2601
jupegarnica <jupegarnica@gmail.com>
2021-04-12 16:09:48 +0200

    refactor in BaseStore

---
+++ src/BaseStore.ts
@@ -0,0 +1,271 @@
+import {
+  calcHash,
+  deepGet,
+  deepSet,
+  getKeys,
+  isValidNumber,
+} from './helpers.ts';
+
+import type {
+  Subscriber,
+  Subscription,
+  Value,
+  Data,
+} from './types.ts';
+/**
+ * A super simple key-value database.
+ * Keys always are strings.
+ * Value type can be specified through generics.
+ */
+export class BaseStore {
+  /**
+   * The file path in which to store the data in.
+   */
+  protected _storePath: string;
+
+  /**
+   * The actual data cache.
+   */
+  protected _data: Data;
+
+  /**
+   * The hashed value of currently cached data.
+   */
+  protected _dataHash: string;
+
+  /**
+   * Stores the last known hash from store file.
+   */
+  protected _lastKnownStoreHash: string;
+
+  protected _subscriptions: Subscription[] = [];
+
+  /**
+   * Create a new {Store} instance.
+   * If no custom path is given, it defaults to mainModulePath/.store.json
+   *
+   * @param storePath A custom path where to write data
+   */
+  constructor(storePath?: string) {
+    this._storePath = storePath
+      ? storePath
+      : `${new URL('.store.json', Deno.mainModule).pathname}`;
+    this._data = {};
+    this._dataHash = '';
+    this._lastKnownStoreHash = '';
+    this.load();
+  }
+  /**
+   * Load stored data from disk into cache.
+   * Won't update cache values if hash in store file matches current cache file.
+   *
+   * @param storePath Custom file path used by read operation
+   * @param force Ignore hash comparison and force read
+   */
+
+
+  protected _notify() {
+    for (const subscription of this._subscriptions) {
+      const { path, callback } = subscription;
+      const value = this.get(path);
+
+      const newHash = calcHash(value);
+      if (newHash !== subscription.hash) {
+        callback(value);
+        subscription.hash = newHash;
+      }
+    }
+  }
+  protected _addDataHash() {
+    this._dataHash = calcHash(this._data);
+  }
+  /**
+   * Retrieves a value from database by specified path.
+   * The path can be an string delimited by . / or \
+   * As:
+   * 'a.b.c'
+   * '/a/b/c' same as 'a/b/c'  or 'a/b/c/'
+   * '\\a\\b\\c'  escaped \
+   *
+   * @param key The key
+   * @returnss The value found or undefined if not found
+   */
+  public get(path: string) {
+    return deepGet(this._data, path);
+  }
+
+  /**
+   * Sets a value in the database by the specified path.
+   * The path can be an string delimited by . / or \
+   * As:
+   * 'a.b.c'
+   * '/a/b/c' same as 'a/b/c'  or 'a/b/c/'
+   * '\\a\\b\\c'  escaped \
+   *
+   * @param path The path
+   * @param value The new value
+   * @returns  The value added
+   *
+   */
+  public set(path: string, value: Value) {
+    deepSet(this._data, path, value);
+
+    this._notify();
+
+    this._addDataHash();
+
+    return value;
+  }
+
+  /**
+   * Remove a value in the database by the specified path.
+   * If pointing to an array item, the item will be remove, as array.splice(index,1)
+   *
+   * @param path The path
+   * @returns  The value removed
+   *
+   */
+  public remove(path: string): Value {
+    const oldValue = this.get(path);
+    const keys = getKeys(path);
+    const lastKey = keys[keys.length - 1];
+
+    if (isValidNumber(lastKey)) {
+      // remove array child
+      keys.pop();
+      const parentValue = this.get(keys.join('.'));
+      parentValue.splice(Number(lastKey), 1);
+    } else {
+      // remove object key
+      deepSet(this._data, path, undefined);
+    }
+
+    this._notify();
+
+    this._addDataHash();
+
+    return oldValue;
+  }
+  /**
+   * Add a new item in an array
+   * It will throw an error if the path is not pointing to an array
+   *
+   * @param path The path
+   * @param value The value
+   * @returns  The value pushed
+   */
+  public push(path: string, value: Value): Value {
+    const oldValue = this.get(path);
+    if (!Array.isArray(oldValue)) {
+      throw new Error('is not an Array');
+    }
+
+    oldValue.push(value);
+
+    this._notify();
+
+    this._addDataHash();
+
+    return value;
+  }
+  /**
+   * Subscribe to changes in the path
+   * It will run the callback if the path value has changed
+   * Also runs the callback during subscription for the first time
+   *
+   * @param path The path
+   * @param callback A function to be called when the value has changed and during subscription
+   * @returns  The value changed
+   */
+  public on(path: string, callback: Subscriber): Value {
+    const value = this.get(path);
+    this._subscriptions.push({
+      callback,
+      hash: calcHash(value),
+      path,
+    });
+    callback(value);
+    return value;
+  }
+
+  /**
+   * Unsubscribe to changes in the path
+   *
+   * @param path The path
+   * @param callback A reference to the callback used in the subscription
+   */
+
+  public off(path: string, callback: Subscriber): void {
+    const oldLength = this._subscriptions.length;
+
+    this._subscriptions = this._subscriptions.filter(
+      (subscription) =>
+        !(
+          subscription.path === path &&
+          subscription.callback === callback
+        ),
+    );
+
+    if (oldLength === this._subscriptions.length) {
+      throw new Error('no subscription found');
+    }
+  }
+
+  /**
+   * Check whether a key is stored inside the database.
+   *
+   * @param key Lookup key
+   * @returnss Whether the key is stored in the database
+   */
+  // TODO: make it work with deep
+  // public contains(key: string): boolean {
+  //   return key in this._data;
+  // }
+
+  // =====================    MANAGEMENT
+
+  /**
+   * Writes cached data to disk.
+   * Won't perform write if the last known hash from the store file
+   * matches the current cache hash.
+   *
+   * @param storePath Custom file path used by write operation
+   * @param force Ignore hashe comparison and force write
+   */
+  public write(storePath?: string, force = false): void {
+    throw new Error("not implemented");
+
+  }
+
+  /**
+   * Deletes a store file / directory.
+   *
+   * @param storePath Custom path used by delete operation. Defaults to the default storage file path
+   */
+  public deleteStore(storePath?: string): void {
+    throw new Error("not implemented");
+
+  }
+  protected load(storePath?: string, force = false): void {
+    throw new Error("not implemented");
+
+  }
+
+  // =====================    GETTER & SETTER
+
+  /**
+   * Return internal storePath.
+   */
+  public get storePath(): string {
+    return this._storePath;
+  }
+
+  // /**
+  //  * Set internal storePath.
+  //  *
+  //  * @param {string} storePath The new path
+  //  */
+  // public set storePath(storePath: string) {
+  //   this._storePath = storePath;
+  // }
+}
--- src/Store.ts
+++ src/Store.ts
@@ -13,49 +13,16 @@ import type {
   Value,
   Data,
 } from './types.ts';
+
+
+import {BaseStore} from './BaseStore.ts';
 /**
  * A super simple key-value database.
  * Keys always are strings.
  * Value type can be specified through generics.
  */
-export class Store {
-  /**
-   * The file path in which to store the data in.
-   */
-  private _storePath: string;
+export class Store extends BaseStore {
 
-  /**
-   * The actual data cache.
-   */
-  private _data: Data;
-
-  /**
-   * The hashed value of currently cached data.
-   */
-  private _dataHash: string;
-
-  /**
-   * Stores the last known hash from store file.
-   */
-  private _lastKnownStoreHash: string;
-
-  private _subscriptions: Subscription[] = [];
-
-  /**
-   * Create a new {Store} instance.
-   * If no custom path is given, it defaults to mainModulePath/.store.json
-   *
-   * @param storePath A custom path where to write data
-   */
-  constructor(storePath?: string) {
-    this._storePath = storePath
-      ? storePath
-      : `${new URL('.store.json', Deno.mainModule).pathname}`;
-    this._data = {};
-    this._dataHash = '';
-    this._lastKnownStoreHash = '';
-    this.load();
-  }
   /**
    * Load stored data from disk into cache.
    * Won't update cache values if hash in store file matches current cache file.
@@ -63,8 +30,9 @@ export class Store {
    * @param storePath Custom file path used by read operation
    * @param force Ignore hash comparison and force read
    */
-  private load(storePath?: string, force = false): void {
+  protected load(storePath?: string, force = false): void {
     if (!storePath) storePath = this._storePath;
+    else this._storePath = storePath;
     if (!existsSync(storePath)) return;
 
     // Load data from file.
@@ -82,165 +50,6 @@ export class Store {
     return;
   }
 
-  private _notify() {
-    for (const subscription of this._subscriptions) {
-      const { path, callback } = subscription;
-      const value = this.get(path);
-
-      const newHash = calcHash(value);
-      if (newHash !== subscription.hash) {
-        callback(value);
-        subscription.hash = newHash;
-      }
-    }
-  }
-  private _addDataHash() {
-    this._dataHash = calcHash(this._data);
-  }
-  /**
-   * Retrieves a value from database by specified path.
-   * The path can be an string delimited by . / or \
-   * As:
-   * 'a.b.c'
-   * '/a/b/c' same as 'a/b/c'  or 'a/b/c/'
-   * '\\a\\b\\c'  escaped \
-   *
-   * @param key The key
-   * @returnss The value found or undefined if not found
-   */
-  public get(path: string) {
-    return deepGet(this._data, path);
-  }
-
-  /**
-   * Sets a value in the database by the specified path.
-   * The path can be an string delimited by . / or \
-   * As:
-   * 'a.b.c'
-   * '/a/b/c' same as 'a/b/c'  or 'a/b/c/'
-   * '\\a\\b\\c'  escaped \
-   *
-   * @param path The path
-   * @param value The new value
-   * @returns  The value added
-   *
-   */
-  public set(path: string, value: Value) {
-    deepSet(this._data, path, value);
-
-    this._notify();
-
-    this._addDataHash();
-
-    return value;
-  }
-
-  /**
-   * Remove a value in the database by the specified path.
-   * If pointing to an array item, the item will be remove, as array.splice(index,1)
-   *
-   * @param path The path
-   * @returns  The value removed
-   *
-   */
-  public remove(path: string): Value {
-    const oldValue = this.get(path);
-    const keys = getKeys(path);
-    const lastKey = keys[keys.length - 1];
-
-    if (isValidNumber(lastKey)) {
-      // remove array child
-      keys.pop();
-      const parentValue = this.get(keys.join('.'));
-      parentValue.splice(Number(lastKey), 1);
-    } else {
-      // remove object key
-      deepSet(this._data, path, undefined);
-    }
-
-    this._notify();
-
-    this._addDataHash();
-
-    return oldValue;
-  }
-  /**
-   * Add a new item in an array
-   * It will throw an error if the path is not pointing to an array
-   *
-   * @param path The path
-   * @param value The value
-   * @returns  The value pushed
-   */
-  public push(path: string, value: Value): Value {
-    const oldValue = this.get(path);
-    if (!Array.isArray(oldValue)) {
-      throw new Error('is not an Array');
-    }
-
-    oldValue.push(value);
-
-    this._notify();
-
-    this._addDataHash();
-
-    return value;
-  }
-  /**
-   * Subscribe to changes in the path
-   * It will run the callback if the path value has changed
-   * Also runs the callback during subscription for the first time
-   *
-   * @param path The path
-   * @param callback A function to be called when the value has changed and during subscription
-   * @returns  The value changed
-   */
-  public on(path: string, callback: Subscriber): Value {
-    const value = this.get(path);
-    this._subscriptions.push({
-      callback,
-      hash: calcHash(value),
-      path,
-    });
-    callback(value);
-    return value;
-  }
-
-  /**
-   * Unsubscribe to changes in the path
-   *
-   * @param path The path
-   * @param callback A reference to the callback used in the subscription
-   */
-
-  public off(path: string, callback: Subscriber): void {
-    const oldLength = this._subscriptions.length;
-
-    this._subscriptions = this._subscriptions.filter(
-      (subscription) =>
-        !(
-          subscription.path === path &&
-          subscription.callback === callback
-        ),
-    );
-
-    if (oldLength === this._subscriptions.length) {
-      throw new Error('no subscription found');
-    }
-  }
-
-  /**
-   * Check whether a key is stored inside the database.
-   *
-   * @param key Lookup key
-   * @returnss Whether the key is stored in the database
-   */
-  // TODO: make it work with deep
-  // public contains(key: string): boolean {
-  //   return key in this._data;
-  // }
-
-  // =====================    MANAGEMENT
 
   /**
    * Writes cached data to disk.
@@ -279,21 +88,4 @@ export class Store {
     return Deno.removeSync(storePath);
   }
 
-  // =====================    GETTER & SETTER
-
-  /**
-   * Return internal storePath.
-   */
-  public get storePath(): string {
-    return this._storePath;
-  }
-
-  /**
-   * Set internal storePath.
-   *
-   * @param {string} storePath The new path
-   */
-  public set storePath(storePath: string) {
-    this._storePath = storePath;
-  }
 }

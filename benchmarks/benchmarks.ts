ba22019373d600c7e777d9eea606f2d31d94caa8
jupegarnica <jupegarnica@gmail.com>
2021-04-22 07:37:36 +0200

    more benchmarks

--- benchmarks/benchmarks.ts
+++ benchmarks/benchmarks.ts
@@ -15,17 +15,20 @@ import { StoreBson } from "../src/StoreBson.ts";
 
 // deno-lint-ignore no-explicit-any
 const d = (time: any) => {
-  if (time > 1000) {
+  if (time > 5000) {
     return duration(time, { format: "second", locale: "en" });
   }
   return time.toFixed(2);
 };
-const RUNS = 1e3;
-1e2;
-1e4;
-1e5;
-1e6;
+const RUNS = Number(Deno.args[0]) ||
+3e6;
 1e7;
+1e6;
+1e5;
+1e4;
+
+1e2;
+1e3;
 
 const resultsOptions = {
   filename: `./results.json`,
@@ -35,16 +38,19 @@ const benchOptions: [
   BenchmarkRunOptions,
   ((progress: BenchmarkRunProgress) => void | Promise<void>),
 ] = [{
-  skip: /Yaml/i,
+  // skip: /Yaml/i,
+  skip: /(autoSave)|(Bson)/i,
   // only: /BSON/,
   // only: /Set/,
   // only: /\[Set\]/,
   // only: /Write/,
   // only: /Load/,
   // only: /(Load)|(Write)/,
+  only: /(Set)|(Load)|(Write)/,
+  // only: /(Find)|(FindOne)|(Set)/,
   // only: /(BSON)|(JSON)/i,
-  silent: true,
-}// (p: BenchmarkRunProgress) => {
+  // silent: true,
+}, () => {} // (p: BenchmarkRunProgress) => {
   // initial progress data
 
   // console.log(p.state);
@@ -66,23 +72,44 @@ bench({
   func(b): void {
     b.start();
     for (let i = 0; i < RUNS; i++) {
-      db.set(`item` + i, i);
+      db.set(`item` + i, {i:{i:{i}}});
+    }
+    b.stop();
+    db.write();
+  },
+});
+bench({
+  name: `[Set Bson]  ${RUNS} children`,
+  runs: 1,
+  func(b): void {
+    const db = new StoreBson({
+      filename: `./data/${RUNS}.bson`,
+    });
+    b.start();
+    for (let i = 0; i < RUNS; i++) {
+      db.set(`item` + i, {i:{i:{i}}});
     }
     b.stop();
     db.write();
   },
 });
 
+
 bench({
-  name: `[Set] ${RUNS} runs`,
-  runs: RUNS,
+  name: `[Set Yaml] ${RUNS} children`,
+  runs: 1,
   func(b): void {
+    const db = new StoreYaml({
+      filename: `./data/${RUNS}.yaml`,
+    });
     b.start();
-    db.set(`item0`, 0);
+    for (let i = 0; i < RUNS; i++) {
+      db.set(`item` + i, {i:{i:{i}}});
+    }
     b.stop();
+    db.write();
   },
 });
-
 bench({
   name: `[Set Json] autoSave ${RUNS} children`,
   runs: 1,
@@ -93,7 +120,7 @@ bench({
     });
     b.start();
     for (let i = 0; i < RUNS; i++) {
-      db.set(`item` + i, i);
+      db.set(`item` + i, {i:{i:{i}}});
     }
     b.stop();
   },
@@ -109,7 +136,7 @@ bench({
     });
     b.start();
     for (let i = 0; i < RUNS; i++) {
-      db.set(`item` + i, i);
+      db.set(`item` + i, {i:{i:{i}}});
     }
     b.stop();
   },
@@ -125,7 +152,7 @@ bench({
     });
     b.start();
     for (let i = 0; i < RUNS; i++) {
-      db.set(`item` + i, i);
+      db.set(`item` + i, {i:{i:{i}}});
     }
     b.stop();
     db.write();
@@ -161,13 +188,42 @@ bench({
   },
 });
 
+// bench({
+//   name: `[Get] ${RUNS} runs`,
+//   runs: RUNS,
+//   func(b): void {
+//     b.start();
+//     db.get(`item0`);
+//     b.stop();
+//   },
+// });
+
+// Find
+//////
+
 bench({
-  name: `[Get] ${RUNS} runs`,
-  runs: RUNS,
+  name: `[Find] ${RUNS} children reading value`,
+  runs: 1,
   func(b): void {
     b.start();
-    db.get(`item0`);
+    const results = db.find("", ([,value]) => value );
     b.stop();
+    if (results.length !== RUNS) {
+      throw new Error(`not ${RUNS} children, ${results.length}`);
+    }
+  },
+});
+
+bench({
+  name: `[Find] ${RUNS} children without reading value`,
+  runs: 1,
+  func(b): void {
+    b.start();
+    const results = db.find("", () => true);
+    b.stop();
+    if (results.length !== RUNS) {
+      throw new Error(`not ${RUNS} children, ${results.length}`);
+    }
   },
 });
 
@@ -184,27 +240,27 @@ bench({
   },
 });
 
-bench({
-  name: `[Load Json] ${RUNS} times`,
-  runs: 1,
-  func(b): void {
-    b.start();
-    for (let i = 0; i < RUNS; i++) {
-      new StoreJson({ filename: `./data/${RUNS}.json` });
-    }
-    b.stop();
-  },
-});
+// bench({
+//   name: `[Load Json] ${RUNS} times`,
+//   runs: 1,
+//   func(b): void {
+//     b.start();
+//     for (let i = 0; i < RUNS; i++) {
+//       new StoreJson({ filename: `./data/${RUNS}.json` });
+//     }
+//     b.stop();
+//   },
+// });
 
-bench({
-  name: `[Load Json] ${RUNS} runs`,
-  runs: RUNS,
-  func(b): void {
-    b.start();
-    new StoreJson({ filename: `./data/${RUNS}.json` });
-    b.stop();
-  },
-});
+// bench({
+//   name: `[Load Json] ${RUNS} runs`,
+//   runs: RUNS,
+//   func(b): void {
+//     b.start();
+//     new StoreJson({ filename: `./data/${RUNS}.json` });
+//     b.stop();
+//   },
+// });
 
 bench({
   name: `[Load Bson]  once ${RUNS} children`,
@@ -216,27 +272,27 @@ bench({
   },
 });
 
-bench({
-  name: `[Load Bson] ${RUNS} times`,
-  runs: 1,
-  func(b): void {
-    b.start();
-    for (let i = 0; i < RUNS; i++) {
-      new StoreBson({ filename: `./data/${RUNS}.bson` });
-    }
-    b.stop();
-  },
-});
+// bench({
+//   name: `[Load Bson] ${RUNS} times`,
+//   runs: 1,
+//   func(b): void {
+//     b.start();
+//     for (let i = 0; i < RUNS; i++) {
+//       new StoreBson({ filename: `./data/${RUNS}.bson` });
+//     }
+//     b.stop();
+//   },
+// });
 
-bench({
-  name: `[Load Bson] ${RUNS} runs`,
-  runs: RUNS,
-  func(b): void {
-    b.start();
-    new StoreBson({ filename: `./data/${RUNS}.bson` });
-    b.stop();
-  },
-});
+// bench({
+//   name: `[Load Bson] ${RUNS} runs`,
+//   runs: RUNS,
+//   func(b): void {
+//     b.start();
+//     new StoreBson({ filename: `./data/${RUNS}.bson` });
+//     b.stop();
+//   },
+// });
 
 bench({
   name: `[Load Yaml]  once ${RUNS} children`,
@@ -248,27 +304,27 @@ bench({
   },
 });
 
-bench({
-  name: `[Load Yaml] ${RUNS} times`,
-  runs: 1,
-  func(b): void {
-    b.start();
-    for (let i = 0; i < RUNS; i++) {
-      new StoreYaml({ filename: `./data/${RUNS}.yaml` });
-    }
-    b.stop();
-  },
-});
+// bench({
+//   name: `[Load Yaml] ${RUNS} times`,
+//   runs: 1,
+//   func(b): void {
+//     b.start();
+//     for (let i = 0; i < RUNS; i++) {
+//       new StoreYaml({ filename: `./data/${RUNS}.yaml` });
+//     }
+//     b.stop();
+//   },
+// });
 
-bench({
-  name: `[Load Yaml] ${RUNS} runs`,
-  runs: RUNS,
-  func(b): void {
-    b.start();
-    new StoreYaml({ filename: `./data/${RUNS}.yaml` });
-    b.stop();
-  },
-});
+// bench({
+//   name: `[Load Yaml] ${RUNS} runs`,
+//   runs: RUNS,
+//   func(b): void {
+//     b.start();
+//     new StoreYaml({ filename: `./data/${RUNS}.yaml` });
+//     b.stop();
+//   },
+// });
 
 // Write
 ////////
@@ -284,18 +340,18 @@ bench({
   },
 });
 
-bench({
-  name: `[Write Json] ${RUNS} times`,
-  runs: 1,
-  func(b): void {
-    new StoreJson({ filename: `./data/${RUNS}.json` });
-    b.start();
-    for (let i = 0; i < RUNS; i++) {
-      db.write();
-    }
-    b.stop();
-  },
-});
+// bench({
+//   name: `[Write Json] ${RUNS} times`,
+//   runs: 1,
+//   func(b): void {
+//     new StoreJson({ filename: `./data/${RUNS}.json` });
+//     b.start();
+//     for (let i = 0; i < RUNS; i++) {
+//       db.write();
+//     }
+//     b.stop();
+//   },
+// });
 
 bench({
   name: `[Write Bson]  once ${RUNS} children`,
@@ -308,18 +364,18 @@ bench({
   },
 });
 
-bench({
-  name: `[Write Bson] ${RUNS} times`,
-  runs: 1,
-  func(b): void {
-    new StoreBson({ filename: `./data/${RUNS}.bson` });
-    b.start();
-    for (let i = 0; i < RUNS; i++) {
-      db.write();
-    }
-    b.stop();
-  },
-});
+// bench({
+//   name: `[Write Bson] ${RUNS} times`,
+//   runs: 1,
+//   func(b): void {
+//     new StoreBson({ filename: `./data/${RUNS}.bson` });
+//     b.start();
+//     for (let i = 0; i < RUNS; i++) {
+//       db.write();
+//     }
+//     b.stop();
+//   },
+// });
 
 bench({
   name: `[Write Yaml]  once ${RUNS} children`,
@@ -332,18 +388,18 @@ bench({
   },
 });
 
-bench({
-  name: `[Write Yaml] ${RUNS} times`,
-  runs: 1,
-  func(b): void {
-    new StoreYaml({ filename: `./data/${RUNS}.yaml` });
-    b.start();
-    for (let i = 0; i < RUNS; i++) {
-      db.write();
-    }
-    b.stop();
-  },
-});
+// bench({
+//   name: `[Write Yaml] ${RUNS} times`,
+//   runs: 1,
+//   func(b): void {
+//     new StoreYaml({ filename: `./data/${RUNS}.yaml` });
+//     b.start();
+//     for (let i = 0; i < RUNS; i++) {
+//       db.write();
+//     }
+//     b.stop();
+//   },
+// });
 
 const { results } = await runBenchmarks(...benchOptions);
 
@@ -377,14 +433,16 @@ for (const result of results) {
 
       const text = `${(data.improvement * 100).toFixed(2)}`;
       const imp = data.improvement > 0 ? colors.red(text) : colors.green(text);
-
+      const toOpsEverySecond = (timeInMs:number, ops:number)=> ops * 1000 / timeInMs
       console.group(colors.dim(name));
       console.debug(
         colors.bold(imp),
-        " ".repeat((7 - text.length)),
+        " ".repeat((9 - text.length)),
+        colors.yellow('x'+diffRatio.toFixed(2)),
         colors.bold(colors.blue(d(measuredRunsAvgMs))),
+        colors.white(`${toOpsEverySecond(measuredRunsAvgMs, RUNS).toFixed(0) }o/s`) ,
         colors.blue(d(averageRun)),
-        colors.yellow(diffRatio.toFixed(2)),
+        colors.brightWhite(`${toOpsEverySecond(averageRun, RUNS).toFixed(0) }o/s`) ,
       );
       console.groupEnd();
 

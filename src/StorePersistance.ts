8faea274576a524380450e394078578f2ee9787a
Jupegarnica <juan@geekshubs.com>
2021-04-26 19:30:51 +0200

    fix type

--- src/StorePersistance.ts
+++ src/StorePersistance.ts
@@ -1,7 +1,7 @@
 import { dirname, existsSync, fromFileUrl, resolve } from "./deps.ts";
 import { Store } from "./Store.ts";
 import { StoreNotFoundError } from "./Errors.ts";
-import type { Config, Value } from "./types.ts";
+import type { Config, Transformation, Value } from "./types.ts";
 import { debounce } from "./helpers.ts";
 /**
  * A database in RAM  with persistance plain text as JSON.

412610c6dad2925e8bb808e0375288a0260ac741
Jupegarnica <juan@geekshubs.com>
2021-05-02 10:10:06 +0200

    comment

--- tests/debug.ts
+++ tests/debug.ts
@@ -1,25 +1,25 @@
-import { Store } from "../src/Store.ts";
-import { assertEquals, assertThrows, delay, Spy, spy } from "./test_deps.ts";
-// import { deepMerge } from "../src/helpers.ts";
-// import { PermissionError, ValidationError } from "../src/Errors.ts";
-// // import { onlyCreate, withTimestamps } from "../src/rulesTemplates.ts";
-// // import { RuleContext, Value } from "../src/types.ts";
+// import { Store } from "../src/Store.ts";
+// import { assertEquals, assertThrows, delay, Spy, spy } from "./test_deps.ts";
+// // import { deepMerge } from "../src/helpers.ts";
+// // import { PermissionError, ValidationError } from "../src/Errors.ts";
+// // // import { onlyCreate, withTimestamps } from "../src/rulesTemplates.ts";
+// // // import { RuleContext, Value } from "../src/types.ts";
 
-const mock: Spy<void> = spy();
+// const mock: Spy<void> = spy();
 
-const db = new Store({ initialData: { a: [1, 2, 3] } });
-db.observe(
-  "a",
-  mock,
-);
-db.beginTransaction();
-db.remove("a.1");
-// db.remove("a.1");
-
-// console.log(
-//   db.get("a"),
+// const db = new Store({ initialData: { a: [1, 2, 3] } });
+// db.observe(
+//   "a",
+//   mock,
 // );
+// db.beginTransaction();
+// db.remove("a.1");
+// // db.remove("a.1");
 
-db.rollback();
+// // console.log(
+// //   db.get("a"),
+// // );
 
-assertEquals(db.get("a"), [1, 2, 3]);
+// db.rollback();
+
+// assertEquals(db.get("a"), [1, 2, 3]);

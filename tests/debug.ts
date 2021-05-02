5c4a2bfa39aed71dba6dd7d43393daf7e9e1e74d
Jupegarnica <juan@geekshubs.com>
2021-05-02 11:33:15 +0200

    lint

--- tests/debug.ts
+++ tests/debug.ts
@@ -1,29 +1,29 @@
-import { Store } from "../src/Store.ts";
-import { assertEquals, assertThrows, delay, Spy, spy } from "./test_deps.ts";
-// // import { deepMerge } from "../src/helpers.ts";
-// // import { PermissionError, ValidationError } from "../src/Errors.ts";
-// // // import { onlyCreate, withTimestamps } from "../src/rulesTemplates.ts";
-// // // import { RuleContext, Value } from "../src/types.ts";
+// import { Store } from "../src/Store.ts";
+// import { assertEquals, assertThrows, delay, Spy, spy } from "./test_deps.ts";
+// // // import { deepMerge } from "../src/helpers.ts";
+// // // import { PermissionError, ValidationError } from "../src/Errors.ts";
+// // // // import { onlyCreate, withTimestamps } from "../src/rulesTemplates.ts";
+// // // // import { RuleContext, Value } from "../src/types.ts";
 
-const db = new Store({
-  rules: {
-    _read: () => true,
-    _write: () => true,
-    set: {
-      _write: (newData) => newData instanceof Set,
-      _transform: (newData) => [...newData],
-      _as: (data) => {
-        console.trace(data);
-        return new Set(data);
-      },
-    },
-  },
-});
-const mySet = new Set([1, 2, 3, 3, 3]);
-// console.log(mySet instanceof Set);
-const returned = db.set("set", mySet);
+// const db = new Store({
+//   rules: {
+//     _read: () => true,
+//     _write: () => true,
+//     set: {
+//       _write: (newData) => newData instanceof Set,
+//       _transform: (newData) => [...newData],
+//       _as: (data) => {
+//         console.trace(data);
+//         return new Set(data);
+//       },
+//     },
+//   },
+// });
+// const mySet = new Set([1, 2, 3, 3, 3]);
+// // console.log(mySet instanceof Set);
+// const returned = db.set("set", mySet);
 
-console.log(
-  // returned,
-  db.get("set"),
-);
+// console.log(
+//   // returned,
+//   db.get("set"),
+// );

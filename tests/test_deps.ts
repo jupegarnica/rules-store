1de6849e1763fda603d72525981f9464600e783c
Jupegarnica <juan@geekshubs.com>
2021-05-04 12:13:30 +0200

    update deps

--- tests/test_deps.ts
+++ tests/test_deps.ts
@@ -2,8 +2,8 @@ export {
   assertEquals,
   assertObjectMatch,
   assertThrows,
-} from "https://deno.land/std@0.93.0/testing/asserts.ts";
+} from "https://deno.land/std@0.95.0/testing/asserts.ts";
 
 export { spy } from "https://deno.land/x/mock@v0.9.5/mod.ts";
 export type { Spy } from "https://deno.land/x/mock@v0.9.5/mod.ts";
-export { delay } from "https://deno.land/std@0.94.0/async/delay.ts";
+export { delay } from "https://deno.land/std@0.95.0/async/delay.ts";

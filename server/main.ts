647e79e52375addf133e5f3a6974590934e4e1a3
Jupegarnica <juan@geekshubs.com>
2021-09-15 17:14:21 +0200

    better logging

--- server/main.ts
+++ server/main.ts
@@ -7,7 +7,7 @@ const app = new Application();
 app.use(async (ctx, next) => {
   await next();
   const rt = ctx.response.headers.get("X-Response-Time");
-  console.log(`${ctx.request.method} ${ctx.request.url} - ${rt}`);
+  console.log(bold(`${ctx.response.status} ${ctx.request.method}`) ,` ${ctx.request.url.pathname}`,yellow(`${rt}`));
 });
 // Timing
 app.use(async (ctx, next) => {
